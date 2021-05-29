65e42ca3ee28559f514298332df305263168c1cb
jet2jet <34238471+jet2jet@users.noreply.github.com>
2021-05-29 16:43:28 +0900

    Remove debug code

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,17 +1,4 @@
-import { generateNotes, makeRandomChord } from './service/chord';
-import { OscillatorPlayer } from './service/player';
-
 import App from './App.svelte';
-
-function getGlobal(this: any): any {
-	if (typeof window !== 'undefined') {
-		return window;
-	} else if (typeof global !== 'undefined') {
-		return global;
-	} else {
-		return this;
-	}
-}
 
 function initialize() {
 	const app = new App({
@@ -19,12 +6,6 @@ function initialize() {
 	});
 	return app;
 }
-
-getGlobal().ChordTest = {
-	generateNotes,
-	makeRandomChord,
-	player: OscillatorPlayer,
-};
 
 if (document.readyState !== 'loading') {
 	initialize();
